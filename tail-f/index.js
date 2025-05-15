a5433b1e7ca00c545c32f2a0c6df3c80732b5d0d
Aasit Vora <aasit@browserstack.com>
2025-05-15 21:11:23 +0530

    MNT: housekeeping

--- tail-f/index.js
+++ tail-f/index.js
@@ -1,8 +1,23 @@
 import http from 'http';
+import fs from 'fs';
+import path from 'path';
 
 const PORT = 8080;
+const __dirname = path.resolve();
+const htmlFilePath = path.join(__dirname, 'index.html');
 
 const server = http.createServer();
+
+server.on('request', (req, res) => {
+  const { method, url } = req;
+
+  if (method === 'GET' && url === '/log') {
+    res.writeHead(200, { 'Content-Type':  'text/html' });
+    res.end(htmlFilePath);
+  } else {
+    res.end('Not Found');
+  }
+});
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
