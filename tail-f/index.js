20ad4d9a445616fc158b760d019ca4ff8fcef256
Aasit Vora <aasit@browserstack.com>
2025-05-16 15:22:25 +0530

    DEV: sock connection init

--- tail-f/index.js
+++ tail-f/index.js
@@ -1,17 +1,21 @@
 import http from 'http';
 import path from 'path';
 import fs from 'fs';
+import { initSocket } from './sock.js';
+import { Server } from 'socket.io';
 
 const PORT = 8080;
 const __dirname = path.resolve();
 
 const server = http.createServer();
+const io = new Server(server);
 
+initSocket(io);
 server.on('request', (req, res) => {
   const { method, url } = req;
 
   if (method === 'GET' && url === '/log') {
-    const stream = fs.createReadStream(path.join(__dirname, 'index.html'));
+    const stream = fs.createReadStream(path.join(__dirname + '/views/index.html'));
     stream.on('error', function() {
         res.writeHead(404);
         res.end();
