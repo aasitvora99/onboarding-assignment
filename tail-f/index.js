e04558f68e1ef5469f7978a382604368e97a91ff
Aasit Vora <aasit@browserstack.com>
2025-05-16 15:22:45 +0530

    MNT: copilot slop

--- tail-f/index.js
+++ tail-f/index.js
@@ -7,11 +7,7 @@ import { Server } from 'socket.io';
 const PORT = 8080;
 const __dirname = path.resolve();
 
-const server = http.createServer();
-const io = new Server(server);
-
-initSocket(io);
-server.on('request', (req, res) => {
+const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   if (method === 'GET' && url === '/log') {
@@ -21,10 +17,25 @@ server.on('request', (req, res) => {
         res.end();
     });
     stream.pipe(res);
+  } else if (method === 'GET' && url === '/client.js') {
+    const stream = fs.createReadStream(path.join(__dirname + '/views/client.js'));
+    stream.on('error', function() {
+        res.writeHead(404);
+        res.end();
+    });
+    res.setHeader('Content-Type', 'application/javascript');
+    stream.pipe(res);
+  } else if (method === 'GET' && url === '/socket.io/socket.io.js') {
+    // Let socket.io handle this request
+    // Do nothing, socket.io will serve its own client
   } else {
+    res.writeHead(404);
     res.end('Not Found');
   }
 });
+
+const io = new Server(server);
+initSocket(io);
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
